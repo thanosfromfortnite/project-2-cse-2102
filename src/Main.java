d9e940d55a627fa7a61c9ddafd366ac024bb62ea
Jesse Han <jhan3537@bths.edu>
2021-11-09 10:31:44 -0500

    added jframe, testing it

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import javax.swing.*;
 import java.util.*;
 
 public class Main {
@@ -7,6 +8,14 @@ public class Main {
 		String directory = System.getProperty("user.dir") + "\\src\\";
 		String fileName = "customers.txt";
 		CustomerProfInterface inter = new CustomerProfInterface(directory + fileName);
+		
+		JFrame frame = new JFrame("Main");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JLabel label = new JLabel("Test");
+		frame.getContentPane().add(label);
+		
+		frame.setSize(800, 600);
+		frame.setVisible(true);
 		
 		while (inter.getUserChoice()) {
 			
